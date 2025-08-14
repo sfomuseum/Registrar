d4d34414434d89c013a4daa6d7b29c808c47ad9e
thisisaaronland <thisisaaronland@localhost>
2025-08-14 12:16:36 -0700

    Initial Commit

---
+++ Registrar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registrar
+//
+//  Created by asc on 8/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RegistrarTests/RegistrarTests.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrarTests.swift
+//  RegistrarTests
+//
+//  Created by asc on 8/14/25.
+//
+
+import Testing
+@testable import Registrar
+
+struct RegistrarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
